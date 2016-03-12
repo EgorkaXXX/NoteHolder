6b367a733555f8404301a479a7b2632e1978d2be
Egor Nedobega <egorkaxxx@gmail.com>
2016-03-12 16:52:13 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,41 +1,5 @@
 window.onload=function(){
 	
-	//Appearence animations with delay
-	
-	setTimeout(function(){
-		
-	document.getElementsByClassName("title")[0].style.color="black";
-	document.getElementsByClassName("right")[0].style.top="0";
 
-	}, 1000);
-	
-	setTimeout(function(){
-		
-	document.getElementsByClassName("tagline")[0].style.color="black";
-	
-		
-	}, 2000);
-	
-	
-	
-	//login animation
-	
-	document.getElementsByClassName("login_btn")[0].onclick=
-		function(){
-	
-	
-		
-	document.getElementsByClassName("title")[0].style.color="transparent";
-		
-	setTimeout(function(){
-		
-	document.getElementsByClassName("tagline")[0].style.color="transparent";
-		
-	}, 1000);
-		
-	document.getElementsByClassName("right")[0].style.top="-1000px";
-	
-	};
-	
 	console.log("script_loaded");
 }
