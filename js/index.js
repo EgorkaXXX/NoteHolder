b227cb9ca9a09eed356c881e475d5311962b5d9e
Egor Nedobega <egorkaxxx@gmail.com>
2016-03-12 17:43:43 +0200

    Update index.js
    start page first commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 window.onload=function(){
 	
-
+	
+	window.onscroll = function(){
+		var scroll = pageYOffset;
+		
+		if(scroll>=105){
+			document.querySelector(".name").style.color="black";
+		}
+		if(scroll<105){
+			document.querySelector(".name").style.color="white";
+		}
+		
+		if(scroll>=10){
+			document.querySelector(".menu").style.boxShadow="0 0 5px lightgray";
+		}
+		if(scroll<10){
+			document.querySelector(".menu").style.boxShadow="0 0 0px lightgray";
+		}
+		
+		
+	console.log(scroll);
+	}
+	
 	console.log("script_loaded");
 }
