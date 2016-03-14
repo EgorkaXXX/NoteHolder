6004bdd6f90c04aa785a52a4269420d1d6e3fc70
Egor Nedobega <egorkaxxx@gmail.com>
2016-03-14 23:09:45 +0200

    Update index.js
    added login alert

--- js/index.js
+++ js/index.js
@@ -16,9 +16,12 @@ var log_menu_btn = document.querySelector(".log_menu_btn");
 var reg_menu_btn = document.querySelector(".reg_menu_btn");
 var log_form = document.querySelector(".log_form");
 var reg_form = document.querySelector(".reg_form");
+var log_btn = document.querySelector(".log_btn");
+var reg_btn = document.querySelector(".reg_btn");	
 var log_cancel_btn =document.querySelector(".log_cancel_btn");
 var reg_cancel_btn =document.querySelector(".reg_cancel_btn");
 var back = document.querySelector(".back");
+var login_alert = document.querySelector(".login_alert");
 	
 	//ON_SCROLL ANIMATIONS
 	window.onscroll = function(){
@@ -52,9 +55,7 @@ var back = document.querySelector(".back");
 	log_cancel_btn.onclick = function(){
 		log_form.style.top="-500px";
 		
-		back.style.opacity="0";
-		setTimeout('document.querySelector(".back").style.zIndex="-1"', 800);
-		
+		back.style.opacity="0";	setTimeout('document.querySelector(".back").style.zIndex="-1"', 800);
 	}
 	
 	//register form
@@ -70,8 +71,7 @@ var back = document.querySelector(".back");
 	reg_cancel_btn.onclick = function(){
 		reg_form.style.top="-500px";
 		
-		back.style.opacity="0";
-		setTimeout('document.querySelector(".back").style.zIndex="-1"', 800);
+		back.style.opacity="0";	setTimeout('document.querySelector(".back").style.zIndex="-1"', 800);
 	}
 	
 	//back click
@@ -79,11 +79,15 @@ var back = document.querySelector(".back");
 		reg_form.style.top="-500px";
 		log_form.style.top="-500px";
 		
-		back.style.opacity="0";
-		setTimeout('document.querySelector(".back").style.zIndex="-1"', 800);
+		back.style.opacity="0";	setTimeout('document.querySelector(".back").style.zIndex="-1"', 800);
 	}
 	
+	//ALERTS
 	
+	//login alert
+	show_login_alert = function(){
+		login_alert.style.bottom="0";	setTimeout('document.querySelector(".login_alert").style.bottom="-80px"', 2000);
+	}
 	
 	console.log("script_loaded");
 }
