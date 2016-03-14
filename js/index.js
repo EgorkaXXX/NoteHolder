cb97a82a7ae2b6ee67ca62ff7d5589d29afa1348
Egor Nedobega <egorkaxxx@gmail.com>
2016-03-15 01:58:20 +0200

    Update index.js
    load anims

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.onload=function(){
+	
 //scss vars
 var dark_indigo =   "#303F9F";
 var indigo = 	 	"#3F51B5";
@@ -10,8 +11,10 @@ var gray =  		"#727272";
 var light =     	"#B6B6B6";
 	
 //elements vars
-var name = document.querySelector(".name");
 var menu = document.querySelector(".menu");
+var logo = document.querySelector(".logo");
+var name = document.querySelector(".name");
+var content = document.querySelector(".content");
 var log_menu_btn = document.querySelector(".log_menu_btn");
 var reg_menu_btn = document.querySelector(".reg_menu_btn");
 var log_form = document.querySelector(".log_form");
@@ -22,6 +25,33 @@ var log_cancel_btn =document.querySelector(".log_cancel_btn");
 var reg_cancel_btn =document.querySelector(".reg_cancel_btn");
 var back = document.querySelector(".back");
 var login_alert = document.querySelector(".login_alert");
+var icon1 = document.querySelector(".icon1");
+var icon2 = document.querySelector(".icon2");
+var icon3 = document.querySelector(".icon3");
+var icon4 = document.querySelector(".icon4");
+var icon5 = document.querySelector(".icon5");
+var icon6 = document.querySelector(".icon6");
+var icon7 = document.querySelector(".icon7");	
+	
+	
+	//PAGE_LOAD APPEARENCE
+	
+	//menu
+	setTimeout(function(){logo.style.opacity=1;},500);
+	setTimeout(function(){log_menu_btn.style.opacity=1;},500);
+	setTimeout(function(){reg_menu_btn.style.opacity=1;},500);
+	
+	//content
+	setTimeout(function(){content.style.opacity=1;},500);
+	
+	//icons
+	setTimeout(function(){icon1.style.opacity=1;},500);
+	setTimeout(function(){icon2.style.opacity=1;},800);
+	setTimeout(function(){icon3.style.opacity=1;},1100);
+	setTimeout(function(){icon4.style.opacity=1;},1400);
+	setTimeout(function(){icon5.style.opacity=1;},1700);
+	setTimeout(function(){icon6.style.opacity=1;},2000);
+	setTimeout(function(){icon7.style.opacity=1;},2300);
 	
 	//ON_SCROLL ANIMATIONS
 	window.onscroll = function(){
@@ -29,11 +59,11 @@ var login_alert = document.querySelector(".login_alert");
 		
 		//name&shadow appearence
 		if(scroll>=15){
-			name.style.color="black";
+			name.style.opacity="1";
 			menu.style.boxShadow="0 0 10px lightgray";
 		}
 		if(scroll<15){
-			name.style.color="white";
+			name.style.opacity="0";
 			menu.style.boxShadow="0 0 0px lightgray";
 		}
 		
@@ -83,7 +113,7 @@ var login_alert = document.querySelector(".login_alert");
 	//ALERTS
 	
 	//login alert
-	show_login_alert = function(){
+	ShowLoginAlert = function(){
 		login_alert.style.bottom="0";	setTimeout('document.querySelector(".login_alert").style.bottom="-80px"', 2000);
 	}
 	
