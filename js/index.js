05df72debf1432dc8eec1a4b82bdef6c13cf3489
Egor Nedobega <egorkaxxx@gmail.com>
2016-03-14 19:01:59 +0200

    Update index.js
    code improvements
    elements defined into vars

--- js/index.js
+++ js/index.js
@@ -3,61 +3,71 @@ window.onload=function(){
 	//ON_SCROLL ANIMATIONS
 	window.onscroll = function(){
 		var scroll = pageYOffset;
+		var name = document.querySelector(".name");
+		var menu = document.querySelector(".menu");
 		
 		//name&shadow appearence
 		if(scroll>=15){
-			document.querySelector(".name").style.color="black";
-			document.querySelector(".menu").style.boxShadow="0 0 10px lightgray";
+			name.style.color="black";
+			menu.style.boxShadow="0 0 10px lightgray";
 		}
 		if(scroll<15){
-			document.querySelector(".name").style.color="white";
-			document.querySelector(".menu").style.boxShadow="0 0 0px lightgray";
+			name.style.color="white";
+			menu.style.boxShadow="0 0 0px lightgray";
 		}
 		
 	}
 	
 	//FORM APPEARENCE\HIDE
+	var log_menu_btn = document.querySelector(".log_menu_btn");
+	var reg_menu_btn = document.querySelector(".reg_menu_btn");
+	var log_form = document.querySelector(".log_form");
+	var reg_form = document.querySelector(".reg_form");
+	var log_cancel_btn = document.querySelector(".log_cancel_btn");
+	var reg_cancel_btn = document.querySelector(".reg_cancel_btn");
+	var back = document.querySelector(".back");
+	
 	
 	//login form
-	document.querySelector(".log_menu_btn").onclick = function(){
-		document.querySelector(".log_form").style.top="150px";
-		document.querySelector(".reg_form").style.top="-500px";
+	log_menu_btn.onclick = function(){
+		log_form.style.top="150px";
+		reg_form.style.top="-500px";
 		
-		document.querySelector(".back").style.opacity="1";
-		document.querySelector(".back").style.zIndex="4";
+		back.style.opacity="1";
+		back.style.zIndex="4";
 	}
 	
-	document.querySelector(".log_cancel_btn").onclick = function(){
-		document.querySelector(".log_form").style.top="-500px";
+	log_cancel_btn.onclick = function(){
+		log_form.style.top="-500px";
 		
-		document.querySelector(".back").style.opacity="0";
+		back.style.opacity="0";
 		setTimeout('document.querySelector(".back").style.zIndex="-1"', 800);
 		
 	}
 	
 	//register form
-	document.querySelector(".reg_menu_btn").onclick = function(){
-		document.querySelector(".reg_form").style.top="150px";
-		document.querySelector(".log_form").style.top="-500px";
+	reg_menu_btn.onclick = function(){
+		reg_form.style.top="150px";
+		log_form.style.top="-500px";
 		
-		document.querySelector(".back").style.opacity="1";
-		document.querySelector(".back").style.zIndex="4";
+		back.style.opacity="1";
+		back.style.zIndex="4";
 		
 	}
 	
-	document.querySelector(".reg_cancel_btn").onclick = function(){
-		document.querySelector(".reg_form").style.top="-500px";
+	reg_cancel_btn.onclick = function(){
+		reg_form.style.top="-500px";
 		
-		document.querySelector(".back").style.opacity="0";
+		back.style.opacity="0";
 		setTimeout('document.querySelector(".back").style.zIndex="-1"', 800);
 	}
 	
 	//back click
-	document.querySelector(".back").onclick = function(){
-		document.querySelector(".reg_form").style.top="-500px";
-		document.querySelector(".log_form").style.top="-500px";
+	back.onclick = function(){
+		reg_form.style.top="-500px";
+		log_form.style.top="-500px";
 		
-		document.querySelector(".back").style.opacity="0";
+		back.style.opacity="0";
 		setTimeout('document.querySelector(".back").style.zIndex="-1"', 800);
 	}
 	
